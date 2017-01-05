12dae53cff5949286a122797c726e3bd786eb235
Ilya Golubkov <ilya.golubkov@gmail.com>
2017-01-05 13:25:00 +0600

    changed init file

--- web/init.sh
+++ web/init.sh
@@ -1,4 +1,2 @@
-sudo unlink /etc/nginx/sites-enabled/default
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-
