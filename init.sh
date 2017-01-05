f1a0d0aee6f518862465767d1097c707c6d60f77
Ilya Golubkov <ilya.golubkov@gmail.com>
2017-01-05 13:15:24 +0600

    Changed nginx config

--- init.sh
+++ init.sh
@@ -1,3 +1,2 @@
-sudo rm /home/etc/nginx/sites-enabled/default
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
