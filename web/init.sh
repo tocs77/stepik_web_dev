78a13252c5295b9f65c7eb70a440ea35131010c5
Ilya Golubkov <ilya.golubkov@gmail.com>
2017-01-12 16:04:08 +0600

    Changed init

--- web/init.sh
+++ web/init.sh
@@ -1,3 +1,7 @@
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+
 sudo /etc/init.d/nginx restart
