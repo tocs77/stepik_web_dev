ff4ff564b18920e557c7e9c7bfad02db8c8c8323
Ilya Golubkov <ilya.golubkov@gmail.com>
2017-01-23 17:24:47 +0600

    Splitted init and start scripts

--- web/init.sh
+++ web/init.sh
@@ -1,7 +1,4 @@
 
+echo "ff"
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-
-sudo /etc/init.d/nginx restart
-sudo /etc/init.d/gunicorn restart
-sudo gunicorn -b 0.0.0.0:8080 hello:app 
---
+++ web/start.sh
@@ -0,0 +1,4 @@
+
+sudo /etc/init.d/nginx restart
+sudo /etc/init.d/gunicorn restart
+sudo gunicorn -b 0.0.0.0:8080 hello:app 
